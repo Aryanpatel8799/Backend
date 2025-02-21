b0711161a77cbdc3ee9ccd77ace7013fa2713c56
Aryanpatel8799 <amp8799@gmail.com>
2025-02-21 10:19:49 +0530

    performing same thing using express.js

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-const http=require('http');
-const server=http.createServer((req,res)=>
+// const http=require('http');
+// const server=http.createServer((req,res)=>
+// {
+//     if(req.url==='/')
+//     {    
+//         res.end('Welcome to our home page');
+//     }
+//     if(req.url=='/about')
+//     {
+//         res.end('Welcome to our about page');
+//     }
+//     if(req.url=='/contact')
+//     {
+//         res.end('Welcome to our contact page');
+//     }
+//     if(req.url=='/profile')
+//     {
+//         res.end('Welcome to our profile page');
+//     }
+// })
+// server.listen(3000);
+
+const express = require('express');
+const app=express();
+
+app.get('/',(req,res)=>
 {
-    if(req.url==='/')
-    {    
-        res.end('Welcome to our home page');
-    }
-    if(req.url=='/about')
-    {
-        res.end('Welcome to our about page');
-    }
-    if(req.url=='/contact')
-    {
-        res.end('Welcome to our contact page');
-    }
-    if(req.url=='/profile')
-    {
-        res.end('Welcome to our profile page');
-    }
+    res.send('Welcome to our home page');
 })
-server.listen(3000);
+app.get('/about',(req,res)=>
+{
+    res.send('Welcome to our about page');
+})
+app.get('/contact',(req,res)=>
+{
+    res.send('Welcome to our contact page');
+})
+app.get('/profile',(req,res)=>
+{
+    res.send('Welcome to our profile page');
+})
+
+
+app.listen(3000);
