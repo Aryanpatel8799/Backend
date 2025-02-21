966c406c93f62dfb89d69b7c9dade23224d851b8
Aryanpatel8799 <amp8799@gmail.com>
2025-02-21 10:08:01 +0530

    routing creation

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http=require('http');
 const server=http.createServer((req,res)=>
 {
-    res.end('hello from server');
+    if(req.url==='/')
+    {    
+        res.end('Welcome to our home page');
+    }
+    if(req.url=='/about')
+    {
+        res.end('Welcome to our about page');
+    }
+    if(req.url=='/contact')
+    {
+        res.end('Welcome to our contact page');
+    }
+    if(req.url=='/profile')
+    {
+        res.end('Welcome to our profile page');
+    }
 })
 server.listen(3000);
